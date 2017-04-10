bf1fe9b7fb1b1350d53ccb2bfc159a2c5bffe96b
brunodahora <brunodahora@trackmob.com.br>
2017-04-10 09:13:04 -0300

    Added redux, react-redux and reactotron.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Reactotron from 'reactotron-react-js';
+import { Provider } from 'react-redux';
+import createStore from './store/createStore';
 
+import './ReactotronConfig';
 import './index.css';
 import Scenery from './components/Scenery';
+
+const store = Reactotron.createStore();
 
 const App = React.createClass({
   render() {
     return (
-      <Scenery/>
+      <Provider store={store}>
+        <Scenery/>
+      </Provider>
     )
   }
 });
